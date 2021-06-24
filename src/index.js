1b0a253e4b4fdafbcdad35c37ee611818e41883d
Ivan Gonzalez <hal308366@gmail.com>
2021-06-23 19:01:36 -0700

    master

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './global.css';
-import App from './components/App';
+import Home from './pages/Home';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<Home/>, container);
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,61 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+import React from 'react';
+import ReactDom from 'react-dom';
 import './styles/Home.css';
-import platziconfLogoImage from '../images/platziconf-logo.svg';
-import astronautsImage from '../images/astronauts.svg';
+import './styles/HomeResponsive.css';
+import HomeMobile from '../assets/images/Mobile.png';
+import TopLogo from '../assets/images/Logo.png';
+import facebook from '../assets/icons/facebook.svg';
+import instagram from '../assets/icons//instagram.svg';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <div className="container">
-          <div className="row">
-            <div className="Home__col col-12 col-md-4">
-              <img
-                src={platziconfLogoImage}
-                alt="Platzi Conf Logo"
-                className="img-fluid mb-2"
-              />
 
-              <h1>Badge Management System</h1>
-              <Link className="btn btn-primary" to="/badges">
-                Start
-              </Link>
-            </div>
-
-            <div className="Home__col d-none d-md-block col-md-8">
-              <img
-                src={astronautsImage}
-                alt="Astronauts"
-                className="img-fluid p-4"
-              />
-            </div>
-          </div>
+class Home extends React.Component {
+    render() {
+        return (
+        <div className="Home__container">
+            <section>
+                <div className="Home__left">
+                    <figure className="figure">
+                        <img className="Home__images" src={HomeMobile} alt="" />
+                    </figure>
+                </div>
+            </section>
+            <section>
+                <div className="Home__right">
+                    <figure className="figure-right">
+                        <img src={TopLogo} width="100" alt="" />
+                    </figure>
+                    <form method="post" action="">
+                        <input className="form-control" type="text" name="" placeholder="Tu nombre de usuario" />
+                        <br></br>
+                        <input className="form-control" type="password" password="" placeholder="Tu contraseña" />
+                        <br></br>
+                        <center>
+                            <button type="button" class="btn btn-info">Entrar</button>
+                        </center>
+                        <br></br>
+                        <p className="text-center">No tienes una cuenta? <a href="" target="_blank">Registrate</a> </p>
+                        <br></br>
+                        <div className="Home__social_bottom">
+                            <div>
+                                <figure>
+                                    <img src={facebook} width="30" alt="Facebook" />
+                                </figure>
+                            </div>
+                            <div>
+                                <figure>
+                                    <img src={instagram} width="30" alt="Instagram" />
+                                </figure>
+                            </div>
+                        </div>
+                        <center>
+                            <small className="Home__footer">Web hecha por WSL</small>
+                        </center>
+                    </form>
+                </div>
+            </section>
         </div>
-      </div>
-    );
-  }
+        );
+    }
 }
+
+export default Home;
